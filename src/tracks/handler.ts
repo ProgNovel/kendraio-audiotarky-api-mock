d24d2519d50861bc5bed0ae1c4a4521021a94cd4
prognovel <prognovel@gmail.com>
2021-03-26 13:32:44 +0800

    t

--- src/tracks/handler.ts
+++ src/tracks/handler.ts
@@ -3,8 +3,12 @@ import { headers } from '../_config';
 export async function handleTracks(event: FetchEvent, page?: string): Promise<Response> {
   const endpoint = `https://www.audiotarky.com/$/tracks
   /${page && parseInt(page) > 1 ? `page/${page}/` : ''}index.json`;
-  const response = await fetch(endpoint);
-  const data = await response.json();
-
-  return new Response(JSON.stringify(data, null, 2), { headers });
+  console.log('fetching:', endpoint);
+  try {
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    return new Response(JSON.stringify(data, null, 2), { headers });
+  } catch (err) {
+    return new Response('Error fetching ' + endpoint, { status: 500, headers });
+  }
 }
