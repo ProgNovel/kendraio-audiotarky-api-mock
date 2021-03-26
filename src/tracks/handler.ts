0cbba0ec9c36431203f5f9d1cb6708afac36f0d2
prognovel <prognovel@gmail.com>
2021-03-26 13:42:16 +0800

    error fix

--- src/tracks/handler.ts
+++ src/tracks/handler.ts
@@ -1,8 +1,9 @@
 import { headers } from '../_config';
 
 export async function handleTracks(event: FetchEvent, page?: string): Promise<Response> {
-  const endpoint = `https://www.audiotarky.com/$/tracks
-  /${page && parseInt(page) > 1 ? `page/${page}/` : ''}index.json`;
+  const endpoint = `https://www.audiotarky.com/$/tracks/${
+    page && parseInt(page) > 1 ? `page/${page}/` : ''
+  }index.json`;
   console.log('fetching:', endpoint);
   try {
     const response = await fetch(endpoint);
